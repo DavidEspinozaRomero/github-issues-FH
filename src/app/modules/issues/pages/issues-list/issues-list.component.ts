c888c5f93ffe877ec66e8da004bc973c28b327e5
David E <deerhou@gmail.com>
2025-05-30 11:38:50 -0500

    Add Issue and Issues List components with basic structure and tests

---
+++ src/app/modules/issues/pages/issues-list/issues-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issues-list',
+  imports: [],
+  templateUrl: './issues-list.component.html',
+  styleUrl: './issues-list.component.css'
+})
+export class IssuesListComponent {
+
+}
